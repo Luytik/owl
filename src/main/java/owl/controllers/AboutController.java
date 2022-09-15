d9e9987f0b7942fd72dfe5b19821b59a5cbb9c0f
vadik <Luytik07@gmail.com>
2022-09-15 22:58:49 +0300

    small fixes in controllers

--- src/main/java/owl/controllers/AboutController.java
+++ src/main/java/owl/controllers/AboutController.java
@@ -1,4 +1,14 @@
 package owl.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/about")
 public class AboutController {
+    @GetMapping
+    public String indexPage() {
+        return "about";
+    }
 }
--- src/main/java/owl/controllers/ContactController.java
+++ src/main/java/owl/controllers/ContactController.java
@@ -1,4 +1,14 @@
 package owl.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/contact")
 public class ContactController {
+    @GetMapping
+    public String indexPage() {
+        return "contact";
+    }
 }
